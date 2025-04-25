fb70e202386b0561b33f410a77d2fe4a10d42f27
Jessa K. West <120423753+jessakwest@users.noreply.github.com>
2025-04-25 16:05:53 -0500

    Create ClientCode.cpp

---
+++ C++/TestScores/ClientCode.cpp
@@ -0,0 +1,35 @@
+/*  Program by Jessa K. West
+    Test Score Program: Client Code
+    
+    Concepts: OOP principles of encapsulation; leverages vectors for dynamic data management
+*/
+
+#include "Class.hpp"
+#include <iomanip>
+
+
+int main() {
+    //format numbers throughout program
+    std::cout << std::fixed << std::setprecision(2);
+
+    std::vector<Student> students = {
+        Student("Anna", 90, 72, 85),
+        Student("Bobby", 60, 78, 82),
+        Student("Cathy", 95, 90, 92),
+        Student("Dave", 60, 70, 80)
+    };
+
+    Class classInfo(students.size());
+
+    for (const auto& student : students) {
+        classInfo.updateGradeDistribution(student.letterGrade);
+    }
+
+    classInfo.calculateClassAverage(students);
+
+    for(const auto& student: students) {
+        student.display();
+    }
+
+    classInfo.displaySummay();
+}
